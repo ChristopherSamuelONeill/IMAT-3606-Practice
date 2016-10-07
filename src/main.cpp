d34d1f7d5bcf774d8cb523c1de27069c37b370a4
unknown <p1415801x@GWHXX633WIN006W.lec-admin.dmu.ac.uk>
2016-10-07 14:26:37 +0100

    FIXED IGNORE ISSUE

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,60 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
+	int iNumbOfGuess = 0;
+	int iRandNumb;
+	bool bPlayerWon = false;
+	int iNumAllowedGuess = 7;
 
+
+	//create random number 0 - 100
+	srand(time(NULL));
+	iRandNumb = rand() % 100 + 0;
+
+	system("CLS");
+	while (iNumbOfGuess < iNumAllowedGuess && bPlayerWon == false)
+	{
+		cout << "Number of guess's remaining " << 7 - iNumbOfGuess << endl;
+		cout << "Guess a number between 0 - 100 : ";
+		int iUserGuess;
+		cin >> iUserGuess;
+		if (iUserGuess > iRandNumb)
+		{
+			cout << "Guess lower" << endl;
+		}
+		if (iUserGuess < iRandNumb)
+		{
+			cout << "Guess higher" << endl;
+		}
+		if (iUserGuess == iRandNumb)
+		{
+			bPlayerWon = true;
+		}
+		if (iNumbOfGuess == iNumAllowedGuess)
+		{
+			bPlayerWon = false;
+		}
+
+		cout << endl << endl;
+		iNumbOfGuess++;
+	}
+
+	if (bPlayerWon) 
+	{
+		system("cls");
+		cout << "Congratulations you guessed the right number " << iRandNumb << endl;
+
+	}
+	else
+	{
+		system("cls");
+		cout << "Unfortunately the correct number was " << iRandNumb << endl;
+	}
+	system("pause");
 	return 0;
 }
 
